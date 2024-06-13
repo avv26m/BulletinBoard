06c29034527f1fc4099cb9256f6bdaae47ae90fd
avv26m <viktor_alferov.1998@bk.ru>
2024-06-13 17:08:29 +0300

    Update

--- project/bullitenboard/models.py
+++ project/bullitenboard/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from ckeditor_uploader.fields import RichTextUploadingField
+
+class Post(models.Model):
+    CATEGORY_CHOICES = [
+        ('tank', 'Танки'),
+        ('heal', 'Хилы'),
+        ('DD', 'ДД'),
+        ('traders', 'Торговцы'),
+        ('gildemasters', 'Гилдмастеры'),
+        ('quest', 'Квестгиверы'),
+        ('blacksmiths', 'Кузнецы'),
+        ('tanners', 'Кожевники'),
+        ('potion', 'Зельевары'),
+        ('spellmasters', 'Мастера заклинаний'),
+    ]
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=12, choices=CATEGORY_CHOICES, default='tank')
+    dateCreation = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255, default='Загаловок')
+    text = models.TextField()
+
+    def __str__(self):
+        return f'{self.id} : {self.title}'
+
+class UserResponse(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    response_time = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(default='Текст отклика')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.author}: {self.text}'
--- project/bullitenboard/views.py
+++ project/bullitenboard/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from .models import Post, UserResponse
 
-# Create your views here.
+class PostList(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Post
+    # Поле, которое будет использоваться для сортировки объектов
+    ordering = '-dateCreation'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+    paginate_by = 2  # количество записей на странице
+
+class UserResponseList(ListView):
+    model = UserResponse
+    ordering = '-response_time'
+    template_name = 'response.html'
+    context_object_name = 'response'
+    paginate_by = 2
+
+class PostDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Post
+    # Используем другой шаблон — product.html
+    template_name = 'post_detail.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'post_detail'
